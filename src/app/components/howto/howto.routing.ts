b14402908457a47607dc536d29784c60073257da
srishti.khandelwal <srishti.khandelwal@PC0307>
2019-04-04 19:24:06 +0530

    docs(doc): messageKey documentation and examples

--- src/app/components/howto/howto.routing.ts
+++ src/app/components/howto/howto.routing.ts
@@ -2,41 +2,45 @@ import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const HOWTO_ROUTES: Routes = [
-{
-	path:'errorMessages',
-	loadChildren: './errorMessages/error-messages.module#ErrorMessagesModule',
-},
-{
-	path:'formData',
-	loadChildren: './formData/form-data.module#FormDataModule',
-},
-{
-	path:'fileObject',
-	loadChildren: './fileObject/file-object.module#FileObjectModule',
-},
-{
-	path:'dirtyCheck',
-	loadChildren: './dirtyCheck/dirty-check.module#DirtyCheckModule',
-},
-{
-	path:'resetForm',
-	loadChildren: './resetForm/reset-form.module#ResetFormModule',
-},
-{
-	path:'stronglyType',
-	loadChildren: './stronglyType/strongly-type.module#StronglyTypeModule',
-},
-{
-	path:'bindConfig',
-	loadChildren: './bindConfig/bind-config.module#BindConfigModule',
-},
-{
-	path:'patchModelValue',
-	loadChildren: './patchModelValue/patch-model-value.module#PatchModelValueModule',
-},
-{
-	path:'logicalOperators',
-	loadChildren: './logicalOperators/logical-operators.module#LogicalOperatorsModule',
-},
+  {
+    path: 'errorMessages',
+    loadChildren: './errorMessages/error-messages.module#ErrorMessagesModule',
+  },
+  {
+    path: 'formData',
+    loadChildren: './formData/form-data.module#FormDataModule',
+  },
+  {
+    path: 'fileObject',
+    loadChildren: './fileObject/file-object.module#FileObjectModule',
+  },
+  {
+    path: 'dirtyCheck',
+    loadChildren: './dirtyCheck/dirty-check.module#DirtyCheckModule',
+  },
+  {
+    path: 'resetForm',
+    loadChildren: './resetForm/reset-form.module#ResetFormModule',
+  },
+  {
+    path: 'stronglyType',
+    loadChildren: './stronglyType/strongly-type.module#StronglyTypeModule',
+  },
+  {
+    path: 'bindConfig',
+    loadChildren: './bindConfig/bind-config.module#BindConfigModule',
+  },
+  {
+    path: 'patchModelValue',
+    loadChildren: './patchModelValue/patch-model-value.module#PatchModelValueModule',
+  },
+  {
+    path: 'logicalOperators',
+    loadChildren: './logicalOperators/logical-operators.module#LogicalOperatorsModule',
+  },
+  {
+    path: 'classAbstraction',
+    loadChildren: './classAbstraction/class-abstraction.module#ClassAbstractionModule',
+  },
 ];
 export const HOWTO_ROUTING: ModuleWithProviders = RouterModule.forChild(HOWTO_ROUTES);
