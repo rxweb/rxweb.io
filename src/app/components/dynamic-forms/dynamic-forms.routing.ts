b9dd429fb33faab4adbc8d1609aa973e464c97f3
srishti.khandelwal <srishti.khandelwal@PC0307>
2019-06-05 12:58:17 +0530

    doc(docs): updated dynamic-form routing

--- src/app/components/dynamic-forms/dynamic-forms.routing.ts
+++ src/app/components/dynamic-forms/dynamic-forms.routing.ts
@@ -18,5 +18,29 @@ const DYNAMIC_FORMS_ROUTES: Routes = [
 	path:'focus',
 	loadChildren: './focus/focus.module#FocusModule',
 },
+{
+	path:'stepbystep',
+	loadChildren: './stepbystep/stepbystep.module#StepbystepModule',
+},
+{
+	path:'textbox',
+	loadChildren: './textbox/textbox.module#TextboxModule',
+},
+{
+	path:'dropdown',
+	loadChildren: './dropdown/dropdown.module#DropdownModule',
+},
+{
+	path:'checkbox',
+	loadChildren: './checkbox/checkbox.module#CheckboxModule',
+},
+{
+	path:'radio',
+	loadChildren: './radio/radio.module#RadioModule',
+},
+{
+	path:'dynamicfile',
+	loadChildren: './dynamicfile/dynamicfile.module#DynamicfileModule',
+},
 ];
 export const DYNAMIC_FORMS_ROUTING: ModuleWithProviders = RouterModule.forChild(DYNAMIC_FORMS_ROUTES);
