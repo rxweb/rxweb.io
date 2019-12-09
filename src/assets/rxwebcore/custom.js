bf308f73a5fa0f360f2895af1a46229d63834926
srishti.khandelwal <srishti.khandelwal@radixweb.com>
2019-12-09 20:50:23 +0530

    web(web):Added rxwebcore in rxweb.io

---
+++ src/assets/rxwebcore/custom.js
@@ -0,0 +1,11 @@
+﻿$(function() {
+    $('#side-menu > li').click(function(e) { // limit click to children of mainmenue
+        var $el = $('ul',this); // element to toggle
+        $('#MainMenu > li > ul').not($el).slideUp(); // slide up other elements
+        $el.stop(true, true).slideToggle(400); // toggle element
+        return false;
+    });
+    $('#MainMenu > li > ul > li').click(function(e) {
+        e.stopPropagation();  // stop events from bubbling from sub menu clicks
+    });
+});​
