98176d89919cdb698af2c2de79edc7aeeb3b9410
ushmi.dave <ushmi.dave@PC0384>
2020-02-12 14:47:45 +0530

    web(web) : modified examples url

--- src/app/components/start/app.component.ts
+++ src/app/components/start/app.component.ts
@@ -71,7 +71,7 @@ export class AppComponent implements OnInit {
   lastRouteName: string = ""
   rightSidebarLinks: any;
   constructor(private router: Router, private applicationBroadCast: ApplicationBroadcaster, private sanitizer: DomSanitizer, private http: HttpClient) {
-    this.gitAsideUrl = sanitizer.bypassSecurityTrustResourceUrl("http://localhost:9999/");
+    this.gitAsideUrl = sanitizer.bypassSecurityTrustResourceUrl("https://rxwebangular.azureedge.net/");
     this.applicationBroadCast.urlSubscriber.subscribe(t => {
       this.homeInit(t)
     });
