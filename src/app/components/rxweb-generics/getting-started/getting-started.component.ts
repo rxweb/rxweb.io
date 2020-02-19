8d5f5511776beda756ab81f9860beb9af06a19c6
ushmi.dave <ushmi.dave@PC0384>
2020-02-19 19:42:30 +0530

    web(web):added getting started sidebar links

--- src/app/components/rxweb-generics/getting-started/getting-started.component.ts
+++ src/app/components/rxweb-generics/getting-started/getting-started.component.ts
@@ -3,30 +3,30 @@ import { ApplicationBroadcaster } from '@rx/core';
 import { Http } from '@angular/http';
 
 @Component({
-    templateUrl: './getting-started.component.html',
-  })
-  export class GenericsGettingStartedComponent implements OnInit {
-    codeContent:any;
+  templateUrl: './getting-started.component.html',
+})
+export class GenericsGettingStartedComponent implements OnInit {
+  codeContent: any;
   showComponent: boolean = false;
-  rightSidebarLinks: any = [{ "id": "getting-started", "title": "Getting Started", "subLink": null },{ "id": "getting-started", "title": "Quick Start", "subLink": null }];  
-  pageTitle:any = this.rightSidebarLinks[0];
+  rightSidebarLinks: any = [{ "id": "getting-started", "title": "Getting Started", "subLink": null }, { "id": "getting-started", "title": "Quick Start", "subLink": null }];
+  pageTitle: any = this.rightSidebarLinks[0];
   titleData: any = { codeContent: {} };
   mainTab: string = "installation"
 
   constructor(private applicationBroadCaster: ApplicationBroadcaster, private http: Http
-    ) {
-      this.applicationBroadCaster.topSubscriber.subscribe(t => {
-        this.titleData = t;
-      })
-    }
+  ) {
+    this.applicationBroadCaster.topSubscriber.subscribe(t => {
+      this.titleData = t;
+    })
+  }
 
   ngOnInit(): void {
     document.title = "Getting Started(Generics) - RxWeb";
     this.http.get('assets/json/generator/rxweb-generics/generic-install.json').subscribe(response => {
       this.codeContent = response.json();
       this.showComponent = true;
-});
+    });
     this.applicationBroadCaster.topSubject.next(this.pageTitle);
   }
-      
-  }
+
+}
