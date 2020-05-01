8118c8645c35b3476572def8c8e22c75e4885c72
srishti.khandelwal <srishti.khandelwal@radixweb.com>
2020-05-01 14:54:04 +0530

    web(web): added getting started for translate

--- src/app/components/rxweb-translate/getting-started/getting-started.component.ts
+++ src/app/components/rxweb-translate/getting-started/getting-started.component.ts
@@ -8,10 +8,28 @@ import { Http } from '@angular/http';
 export class TranslateGettingStartedComponent implements OnInit {
 
   showComponent: boolean = false;
-  rightSidebarLinks: any = [{ "id": "getting-started", "title": "Quick Start", "subLink": null }, { "id": "getting-started", "title": "Quick Start", "subLink": null }];
+  rightSidebarLinks: any = [{ "id": "getting-started", "title": "Getting Started", "subLink": null }, { "id": "getting-started", "title": "Getting Started", "subLink": null }];
   pageTitle: any = this.rightSidebarLinks[0];
   titleData: any = { codeContent: {} };
   mainTab: string = "installation"
+  importModule: any = `RxTranslateModule.forRoot({
+          cacheLanguageWiseObject: true,
+          globalFilePath: "assets/i18n/{{language-code}}/global.{{language-code}}.json",
+          filePath: "assets/i18n/{{language-code}}/{{translation-name}}.{{language-code}}.json"
+      })`
+  mapTranslate: any = `@translate() 
+multilingualContent: { [key: string]: any }`
+
+  enJsonFile: any = `{
+  "pageWiseTitle": "Resolve Translation Page Wise",
+  "placeholder": {
+    "firstName": "Enter your first name"
+  },
+  "text": {
+    "firstName": "First Name",
+    "create": "Create Account"
+  }
+}`
 
   constructor(private applicationBroadCaster: ApplicationBroadcaster, private http: Http
   ) {
