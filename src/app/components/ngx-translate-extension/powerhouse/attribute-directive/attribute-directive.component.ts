c423f43c522b54dbf55dfa0f911d8a33000adc02
ajayojha <ojhaajay@outlook.com>
2020-05-27 02:34:24 +0530

    add ngx-translate-extension documentation

--- src/app/components/ngx-translate-extension/powerhouse/attribute-directive/attribute-directive.component.ts
+++ src/app/components/ngx-translate-extension/powerhouse/attribute-directive/attribute-directive.component.ts
@@ -1,5 +1,6 @@
 import { OnInit, Component } from '@angular/core';
-
+declare const Prism;
+declare const $;
 @Component({
   templateUrl: './attribute-directive.component.html',
 })
@@ -7,9 +8,20 @@ export class AttributeDirectiveComponent implements OnInit {
 
   rightSidebarLinks: any = [{ "id": "attribute-directive", "title": "Attribute Directive", "subLink": null }, { "id": "attribute-directive", "title": "Attribute Directive", "subLink": null }];
   outputHtml: string;
+    examples: any = {
+        first: {
+            json: `{
+"firstName":"Enter Your First Name"
+}`,
+            html: `<span translate="firstName"></span>`
+        }
+    }
 
-  ngOnInit() {
-
+    json: any;
+    html: any;
+    ngOnInit() {
+        this.json = Prism.highlight(this.examples.first.json, Prism.languages['json'], 'json');
+        this.html = Prism.highlight(this.examples.first.html, Prism.languages['html'], 'html');
   }
 
 }
