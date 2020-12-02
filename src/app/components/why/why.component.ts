9bfd29ddb0594067cdc110e87e501175c6bd0fcf
ushmi.dave <ushmi.dave@PC0384>
2020-12-02 20:17:49 +0530

    web : update why doc

--- src/app/components/why/why.component.ts
+++ src/app/components/why/why.component.ts
@@ -13,28 +13,48 @@ export class WhyComponent implements OnInit {
     codeContent: any;
     selectedData: any = "Reactive Forms";
     displayData : string = "";
+    listItems:any=[];
+    activeListItem:string="All";
     constructor(private http: HttpClient, private activateRoute: ActivatedRoute) {
         activateRoute.queryParams.subscribe(t => {
-            debugger;
             this.selectedData = t;
             if(this.selectedData == "reactive-forms")
             this.displayData = "Dynamic Forms"
             else
             this.displayData = "Reactive Forms"
         })
-
     }
+
     ngOnInit() {
         document.title = "Why - RxWeb Docs";
-        this.http.get(`assets/json/generator/why/${this.selectedData.package}.json`).subscribe(response => {
-            this.codeContent = response;
+        this.http.get(`assets/json/generator/why/${this.selectedData.package}.json`).subscribe(response => {            
+            this.codeContent = response;  
+            if(this.codeContent)
+            this.codeContent.forEach(element => {
+                this.listItems.push(element.title);
+            });        
         });
     }
 
     loadWhyData(packageName: string) {
+        this.listItems = [];
         this.http.get(`assets/json/generator/why/${packageName}.json`).subscribe(response => {
             this.codeContent = response;
+            if(this.codeContent)
+            this.codeContent.forEach(element => {
+                this.listItems.push(element.title);
+            });
         });
     }
 
+    scrollTo(section) {
+        var node = document.querySelector('#' + section);
+        node.scrollIntoView(true);
+        var scrolledY = window.scrollY;
+        if (scrolledY) {
+          window.scroll(0, scrolledY - 62);
+        }
+        return false;
+      }
+   
 }
