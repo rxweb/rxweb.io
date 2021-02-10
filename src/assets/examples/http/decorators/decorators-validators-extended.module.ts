8ee1f202b0a6c0f05c17bfb6eb73867205101756
ushmi.dave <ushmi.dave@PC0384>
2021-02-10 13:02:09 +0530

    web : remove http decorator component

--- src/assets/examples/http/decorators/decorators-validators-extended.module.ts
+++ src/assets/examples/http/decorators/decorators-validators-extended.module.ts
@@ -2,15 +2,14 @@ import { CommonModule } from '@angular/common';
 import { NgModule,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { RxReactiveFormsModule } from '@rxweb/reactive-form-validators';
-import { DecoratorsHttpComponent } from './http/decorators-http.component';
+
 
 @NgModule({
   declarations: [
-    DecoratorsHttpComponent
+    
   ],
 entryComponents: [
-  DecoratorsHttpComponent
+  
   ],
   imports: [
     CommonModule,HttpClientModule,ReactiveFormsModule,FormsModule,RxReactiveFormsModule
@@ -19,7 +18,7 @@ entryComponents: [
   bootstrap: [],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   exports: [
-    DecoratorsHttpComponent
+    
   ],
 
 })
